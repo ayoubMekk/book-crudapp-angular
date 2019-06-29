12b08f9fd8eed5fa1e5796fceb2d1e335e4e90fc
Ayoub Mekkaoui <ayoubsoft2011@hotmail.fr>
2019-06-29 03:14:10 +0200

    fix router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,14 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
-const logged = localStorage.getItem("iduser")
-const homeComponent = logged != null ? HomeComponent : LoginComponent
-console.log("logged ?" +logged)
+
 
 const routes: Routes = [
-  { path:"", component: logged != null ? HomeComponent : LoginComponent},
+  { path:"", component: localStorage.getItem("iduser") != null ? HomeComponent : LoginComponent},
   { path:"register", component: RegisterComponent}, 
-  { path:"login" , component:logged != null ? HomeComponent : LoginComponent},
-  { path:"add", component:logged != null ?  AddComponent :LoginComponent},
-  { path:"edit/:id", component:  logged != null ? EditComponent :LoginComponent},
+  { path:"login" , component:  LoginComponent},
+  { path:"add", component:localStorage.getItem("iduser") != null ?  AddComponent :LoginComponent},
+  { path:"edit/:id", component:  localStorage.getItem("iduser") != null ? EditComponent :LoginComponent},
   { path:"contact", component: AboutComponent},
   { path:"**", component: PagenotfoundComponent},
 ];
