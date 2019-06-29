7456c1a6a77ec91d359294530b9611ada99f71e3
Ayoub Mekkaoui <ayoubsoft2011@hotmail.fr>
2019-06-29 02:41:30 +0200

    fix dist path

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/book-crudapp-angular'));
+app.use(express.static(__dirname + '/book-crudapp-angular'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/book-crudapp-angular/index.html'));
+  res.sendFile(path.join(__dirname + '/book-crudapp-angular/index.html'));
 });
 
 // default Heroku PORT
